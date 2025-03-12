1c97f10778e8828449604cdc9335d69ace0f06d8
jayeshphalea <jayeshphalea@gmail.com>
2025-03-12 12:45:28 +0530

    Initial commit

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -19,14 +19,14 @@ class Counter extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Count: {this.state.count}</h1>
-        <button onClick={this.increment} className="btn increment">
+      <div className="counter-container">
+        <h1 className="counter-display">Count: {this.state.count}</h1>
+        <button className="btn increment" onClick={this.increment}>
           ➕ Increment
         </button>
         <button
-          onClick={this.decrement}
           className="btn decrement"
+          onClick={this.decrement}
           disabled={this.state.count === 0}
         >
           ➖ Decrement
